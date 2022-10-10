e4018746f97f0a23d7e3db69d767210275934e53
sampaiorafael <rafasampaio021@hotmail.com>
2022-10-10 20:20:40 -0300

    feat: init swagger & open server on .env port

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import SwaggerConfig from './configs/swagger.config';
+require('dotenv/config');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  SwaggerConfig.init(app);
+  app.enableCors();
+  await app.listen(process.env.PORT || 8080);
 }
 bootstrap();
