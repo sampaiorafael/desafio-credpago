7be6f446d165a7dda3b7bf62b98d416c4bcf2d47
sampaiorafael <rafasampaio021@hotmail.com>
2022-10-12 14:22:51 -0300

    feat: find last credit by date also

--- src/modules/credit_analysis/credit-analysis.repository.ts
+++ src/modules/credit_analysis/credit-analysis.repository.ts
@@ -1,4 +1,4 @@
-import { Repository } from 'typeorm';
+import { Repository, MoreThan } from 'typeorm';
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm'
 import { CreditAnalysisEntity } from './credit-analysis.entity';
@@ -12,9 +12,12 @@ export class CreditAnalysisRepository {
     private readonly creditAnalysisRepository: Repository<CreditAnalysisEntity>
   ) {}
 
-  public async findLastCreditAnalysisByCpf(cpf: string): Promise<CreditAnalysisEntity> {
+  public async findLastCreditAnalysisByCpfFromDate(cpf: string, date: Date = new Date(0)): Promise<CreditAnalysisEntity> {
     const lastCreditAnalysis = await this.creditAnalysisRepository.findOne({
-      where: { cpf },
+      where: { 
+        cpf,
+        createdAt: MoreThan(date)
+      },
       order: { createdAt: 'DESC' }
     })
     return lastCreditAnalysis
