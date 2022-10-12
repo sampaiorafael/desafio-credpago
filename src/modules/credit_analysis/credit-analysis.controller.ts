ad343cc2877fb9203959892621629bb973f64438
sampaiorafael <rafasampaio021@hotmail.com>
2022-10-12 16:32:39 -0300

    feat: swagger response route type

--- src/modules/credit_analysis/credit-analysis.controller.ts
+++ src/modules/credit_analysis/credit-analysis.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiBody, ApiTags } from '@nestjs/swagger';
-import { CreditAnalysisRequestDTO } from './dtos/credit-analysis.dto';
+import { ApiBody, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreditAnalysisRequestDTO, CreditAnalysisResponseDTO } from './dtos/credit-analysis.dto';
 import { CreditAnalysisService } from './resources/credit-analysis/credit-analysis.service';
 
 @Controller()
@@ -13,6 +13,7 @@ export class CreditAnalysisController {
 
   @Post()
   @ApiBody({ type: CreditAnalysisRequestDTO })
+  @ApiResponse({ type: CreditAnalysisResponseDTO })
   public async creditAnalysis(
     @Body() payload: CreditAnalysisRequestDTO
   ): Promise<any> {
