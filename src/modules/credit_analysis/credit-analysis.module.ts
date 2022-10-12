a8b849f696453c56ec3c60a9041e88d3852cd584
sampaiorafael <rafasampaio021@hotmail.com>
2022-10-12 13:55:13 -0300

    chore: add entity to module

--- src/modules/credit_analysis/credit-analysis.module.ts
+++ src/modules/credit_analysis/credit-analysis.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { CepModule } from '../cep/cep.module';
 import { CreditAnalysisController } from './credit-analysis.controller';
+import { CreditAnalysisEntity } from './credit-analysis.entity';
+import { CreditAnalysisRepository } from './credit-analysis.repository';
 import { CreditAnalysisService } from './resources/credit-analysis/credit-analysis.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 
 @Module({
-  imports: [CepModule],
+  imports: [
+    TypeOrmModule.forFeature([CreditAnalysisEntity]),
+    CepModule
+  ],
   controllers: [CreditAnalysisController],
-  providers: [CreditAnalysisService],
+  providers: [
+    CreditAnalysisRepository,
+    CreditAnalysisService,
+  ],
 })
 export class CreditAnalysisModule {}
