55cd0c214b435ea1383dbe7834338abe4856606e
sampaiorafael <rafasampaio021@hotmail.com>
2022-10-12 13:42:43 -0300

    chore: typeorm configs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CepModule } from './modules/cep/cep.module';
 import { CreditAnalysisModule } from './modules/credit_analysis/credit-analysis.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeormConfigs } from './configs/typeorm.config';
+ 
 @Module({
   imports: [
+    TypeOrmModule.forRoot(typeormConfigs),
     CepModule,
     CreditAnalysisModule,
   ],
