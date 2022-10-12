19eea61e38d34cc157838491f2946c3527461b1e
sampaiorafael <rafasampaio021@hotmail.com>
2022-10-12 12:33:33 -0300

    refactor: change dto dir

--- src/modules/credit_analysis/dtos/credit-analysis.dto.ts
+++ src/modules/credit_analysis/dtos/credit-analysis.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from "@nestjs/swagger";
 
-export class CreditAnalysisDTO {
+export class CreditAnalysisRequestDTO {
   @ApiProperty({
     description: 'User name',
     default: 'Rafael',
