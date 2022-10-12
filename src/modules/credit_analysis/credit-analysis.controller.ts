e361e3d0636560cc937a638f88863566e8da73e3
sampaiorafael <rafasampaio021@hotmail.com>
2022-10-12 14:23:03 -0300

    feat: credit analysis

--- src/modules/credit_analysis/credit-analysis.controller.ts
+++ src/modules/credit_analysis/credit-analysis.controller.ts
@@ -1,13 +1,23 @@
-import { Controller, Post } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiBody, ApiTags } from '@nestjs/swagger';
+import { CreditAnalysisRequestDTO } from './dtos/credit-analysis.dto';
+import { CreditAnalysisService } from './resources/credit-analysis/credit-analysis.service';
 
 @Controller()
 @ApiTags('credit-analysis')
 export class CreditAnalysisController {
 
+  constructor(
+    private readonly creditAnalysisService: CreditAnalysisService
+  ) {}
+
   @Post()
-  getHello(): string {
-    return 'Hello'
+  @ApiBody({ type: CreditAnalysisRequestDTO })
+  public async creditAnalysis(
+    @Body() payload: CreditAnalysisRequestDTO
+  ): Promise<any> {
+    const userCreditAnalysis = await this.creditAnalysisService.creditAnalysis(payload)
+    return userCreditAnalysis
   }
 
 }
---
+++ src/modules/credit_analysis/resources/credit-analysis/credit-analysis.service.ts
@@ -0,0 +1,67 @@
+import { BadRequestException, Inject, Injectable } from '@nestjs/common';
+import { CepService } from 'src/modules/cep/cep.service';
+import { inflateRaw } from 'zlib';
+import { CreditAnalysisResult } from '../../credit-analysis-result.enum';
+import { CreditAnalysisRepository } from '../../credit-analysis.repository';
+import { CreditAnalysisRequestDTO, CreditAnalysisResponseDTO } from '../../dtos/credit-analysis.dto';
+
+@Injectable()
+export class CreditAnalysisService {
+
+  constructor(
+    @Inject(CreditAnalysisRepository)
+    private readonly creditAnalysisRepository: CreditAnalysisRepository,
+    private readonly cepService: CepService
+  ) {}
+
+  public async creditAnalysis(data: CreditAnalysisRequestDTO): Promise<CreditAnalysisResponseDTO> {
+    const cepVerification = await this.cepService.verifyCep(data.cepCodeAddress)
+
+    if(
+      !cepVerification || 
+      cepVerification.uf != data.stateAddress || 
+      cepVerification.localidade != data.cityAddress
+    ) throw new BadRequestException('Was not possible to validate this CEP code')
+
+    let creditScore = 100
+
+    const rentValueIsHigher = this.rentValueVerification(data.rentValue, data.income)
+    const lastCreditAnalysisIn90Days = await this.creditAnalysisRepository.findLastCreditAnalysisByCpfFromDate(data.cpf)
+
+    if(rentValueIsHigher) creditScore = this.removePercentageFromScore(creditScore, 18)
+    if(data.badCreditReputation) creditScore = this.removePercentageFromScore(creditScore, 31)
+    if(data.creditCardLimit <= data.rentValue) creditScore = this.removePercentageFromScore(creditScore, 15)
+    if(lastCreditAnalysisIn90Days) {
+      if(lastCreditAnalysisIn90Days.result === CreditAnalysisResult.DENIED) creditScore = this.removePercentageFromScore(creditScore, 10)
+    }
+    
+    const roundedScore = Math.ceil(creditScore)
+
+    const result = (rentValueIsHigher && data.badCreditReputation) ? 
+      CreditAnalysisResult.DENIED : 
+      this.defineCreditAnalysisScoreResult(roundedScore)
+
+    const completeResult = await this.creditAnalysisRepository.createCreditAnalysis(data.cpf, result)
+
+    return {
+      id: completeResult.id,
+      score: roundedScore,
+      result
+    }
+  }
+
+  private defineCreditAnalysisScoreResult(score: number): CreditAnalysisResult {
+    if(score <= 30) return CreditAnalysisResult.DENIED
+    if(score > 30 && score < 60) return CreditAnalysisResult.TOMANUAL
+    if(score >= 60) return CreditAnalysisResult.APPROVED
+  }
+
+  private rentValueVerification(rentValue: number, income: number): boolean {
+    return rentValue > (income * 0.30)
+  }
+
+  private removePercentageFromScore(score: number, percentage: number): number {
+    const percentageValue = (score * percentage / 100)
+    return score - percentageValue
+  }
+}
